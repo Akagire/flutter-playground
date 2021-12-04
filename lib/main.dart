0f423bb76f40ec875468cc54771c8502119859ad
Takuya Eguchi <seal2501@gmail.com>
2021-12-04 13:59:38 +0900

    feat: 自分自身が状態管理しているウィジェットのチュートリアル https://docs.flutter.dev/development/ui/interactive#the-widget-manages-its-own-state

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
-import './layout_demo.dart';
+// import './layout_demo.dart';
+
+class TapboxA extends StatefulWidget {
+  const TapboxA({Key? key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const LayoutApp();
+    // return const LayoutApp();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: TapboxA(),
+        ),
+      ),
+    );
   }
 }
 
