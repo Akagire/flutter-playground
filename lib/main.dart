89c57d65c5bcc7e25027d0c8c802050705c41552
Takuya Eguchi <seal2501@gmail.com>
2021-12-05 17:40:46 +0900

    feat: 追加ページへの遷移を実装するため、main.dartに記述していたコンポーネントをtop.dartに切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,12 @@
 import 'package:flutter/material.dart';
-import './theme/app_theme.dart';
-import './organisms/palette.dart';
-
-class MyApp extends StatelessWidget {
-  final colors = [
-    [
-      const Color(0xffffffff),
-      const Color(0xffbc002d),
-      const Color(0xffdfe3e2),
-      const Color(0xff7b807a),
-      const Color(0xff130f0c),
-    ],
-    [
-      const Color(0xff4d3614),
-      const Color(0xffb69b63),
-      const Color(0xffedeaec),
-      const Color(0xff6d4845),
-      const Color(0xffac291d),
-      const Color(0xff00426d),
-    ],
-    [
-      const Color(0xffffec00),
-      const Color(0xff9cc45a),
-      const Color(0xfffadce5),
-      const Color(0xffecaeca),
-      const Color(0xff797d8a),
-    ],
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    int _selectedIndex = 0;
-    return MaterialApp(
-      title: 'Memorize Color',
-      theme: AppTheme().lightTheme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Colors'),
-        ),
-        body: ListView(
-          children: [
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-              Palette(title: 'Sample', colors: colors[0]),
-              Palette(title: 'Sample', colors: colors[1]),
-              Palette(title: 'Sample', colors: colors[2]),
-            ]),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('wip');
-          },
-          child: const Icon(Icons.add, color: Colors.black54),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      ),
-    );
-  }
-}
+import '../theme/app_theme.dart';
+import './pages/top.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'Memorize Color',
+    theme: AppTheme().lightTheme,
+    home: TopPage(),
+  ));
 }
---
+++ lib/pages/new.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add your color'),
+      ),
+      body: Center(
+        child: Text('WIP'),
+      ),
+    );
+  }
+}
