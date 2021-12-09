5cf05e4265046fcd3da115994dc3647534e3d000
Takuya Eguchi <seal2501@gmail.com>
2021-12-09 22:42:23 +0900

    feat: AnimatedBuilderによるリファクタ https://docs.flutter.dev/development/ui/animations/tutorial#refactoring-with-animatedbuilder

--- lib/logo.dart
+++ lib/logo.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AnimatedLogo extends AnimatedWidget {
-  const AnimatedLogo({Key? key, required Animation<double> animation})
-      : super(key: key, listenable: animation);
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: const FlutterLogo(),
+    );
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  const GrowTransition({required this.child, required this.animation, Key? key})
+      : super(key: key);
+
+  final Widget child;
+  final Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return SizedBox(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
       ),
     );
   }
@@ -48,7 +67,9 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   }
 
   @override
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+  Widget build(BuildContext context) {
+    return GrowTransition(child: const LogoWidget(), animation: animation);
+  }
 
   @override
   void dispose() {
